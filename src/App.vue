4306366aec893e6c3f8065a1d2a3636cbdc2bafd
veenacc <veena.chenicheri@gmail.com>
2024-06-25 12:13:45 -0700

    initialized chair-vue project,initial set up

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import Header from "./components/Header.vue";
+import Content from "./components/Content.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Content,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+</template>
+
+<style></style>
